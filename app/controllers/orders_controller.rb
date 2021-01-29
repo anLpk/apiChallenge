9ea13a0592ef9fbd22dd413e3d3bba60273ec084
anil pak <anilpak35@gmail.com>
2021-01-29 17:05:09 +1100

    scss created, started writing HTML and SCSS

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,15 @@ class OrdersController < ApplicationController
   def show
   end
 
-  private
   class RealHubApiKey
+    include HTTParty
+
     BASE_URL = "https://app.realhublive.com/api/v2"
     API_PARTIAL_URL = "api-key=#{ENV["realHub_API_KEY"]}"
+    
+    def response
+      request = HTTParty.get(BASE_URL+"campaigns.json"+API_PARTIAL_URL).to_json
+      @request_hash = JSON.parse(request)
+    end
   end
 end
