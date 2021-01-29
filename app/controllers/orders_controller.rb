d63fd30c2cbaeb5e591f415f942ee392088cc12c
anil pak <anilpak35@gmail.com>
2021-01-29 16:07:25 +1100

    httparty gem installed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,12 @@ require 'open-uri'
 require 'json'
 
 class OrdersController < ApplicationController
-    def show
-    end
+  def show
+  end
+
+  private
+  class RealHubApiKey
+    BASE_URL = "https://app.realhublive.com/api/v2"
+    API_PARTIAL_URL = "api-key=#{ENV["realHub_API_KEY"]}"
+  end
 end
